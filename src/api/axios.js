19e39936723fac584775b2e97cea9110a16ca130
leonardo.assuncao <leonardo.assuncao>
2021-04-27 09:31:18 -0300

    c

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,9 @@ import axios from 'axios'
 
 const baseURL = process.env.JOGO_FORCA_API || 'http://localhost:7000'
 
+console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+console.log(baseURL)
+
 export default axios.create({
     baseURL: baseURL
 })
