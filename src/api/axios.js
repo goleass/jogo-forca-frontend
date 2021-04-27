65319d80c82fe2354ce42e1e32636e5b89ea49b6
leonardo.assuncao <leonardo.assuncao>
2021-04-27 08:06:24 -0300

    add api servida

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from 'axios'
 
+const baseURL = process.env.JOGO_FORCA_API || 'http://localhost:7000/users/'
+
 export default axios.create({
-    baseURL: 'http://localhost:7000/users/'
+    baseURL: baseURL
 })
