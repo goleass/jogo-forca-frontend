c6c050d46ba3f550219cffd6afd8e65282488b86
leonardo.assuncao <leonardo.assuncao>
2021-05-03 22:43:57 -0300

    corrigindo exbir ranking

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './routes'
-import Jogo from './views/Jogo'
 
 function App() {
 
   return (
     <>
-      <Jogo />
       <BrowserRouter>
         <Routes />
         {/* <Footer /> */}
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
 import User from '../views/User'
+import Category from '../views/Categories'
+import Words from '../views/Words'
 import Home from '../views/Home'
+import Jogo from '../views/Jogo'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
 
     return (
         <Switch>
+            <Route exact path='/'><Jogo /></Route>
             <Route exact path='/admin'><Home /></Route>
             <Route exact path='/admin/usuarios'><User /></Route>
-            <Route exact path='/admin/categorias'>categorias</Route>
-            <Route exact path='/admin/palavras'>palavras</Route>
+            <Route exact path='/admin/categorias'><Category /></Route>
+            <Route exact path='/admin/palavras'><Words /></Route>
             {/* <Route path='/'>antes</Route> */}
         </Switch>
     )
