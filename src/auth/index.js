468cae0958ebab91b51a4b4b96e2a3ea91a9c08d
leonardo.assuncao <leonardo.assuncao>
2021-06-07 19:55:49 -0300

    login

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,3 +1,3 @@
 export const isAutenticated = () => {
-    return true
+    return true && localStorage.getItem('app-token')
 }
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,17 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Container, Form, Row } from 'react-bootstrap'
+
+import axios from '../../api/axios'
 
 import './styles.css'
 
+const base = process.env.NODE_ENV==='production'?'https://forca-jogo.herokuapp.com':'https://forca-jogo.herokuapp.com'
+
+
 const Login = () => {
+    const [user, setUser] = useState();
+    const [pass, setPass] = useState();
+
+    const handleUser = e => {
+        setUser(e.target.value)
+    }
+
+    const handlePass = e => {
+        setPass(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        
+        const data = {
+            "usuario": user,
+            "senha": pass
+        }
+
+        axios.post('/authenticate/auth', data).then(r => {
+            if(r.data && r.data.token){
+                const token = r.data.token
+                localStorage.setItem('app-token', token)
+                window.location.href=`${base}/admin/`
+            }
+
+        }).catch(e => console.log(e))
+    }
+
     return (
         <Container>
             <Row className="justify-content-center">
                 <Form.Group>
                     <h2 className="mt-5">Login</h2>
-                    <Form.Control placeholder="usuário" className=""/>
-                    <Form.Control placeholder="senha" className="mt-2"/>
-                    <Button block className="mt-2">login</Button>
+                    <Form.Control onChange={handleUser} value={user} placeholder="usuário" className=""/>
+                    <Form.Control onChange={handlePass} value={pass} placeholder="senha" className="mt-2"/>
+                    <Button onClick={() => handleSubmit()} block className="mt-2">login</Button>
                 </Form.Group>
             </Row>
         </Container>
