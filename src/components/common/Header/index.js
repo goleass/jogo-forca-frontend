ff01b765505bbf39349490fe4ba65a5110b7b37b
leonardo.assuncao <leonardo.assuncao>
2021-06-07 19:59:09 -0300

    vdf

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -1,7 +1,15 @@
 import { Button,Navbar,Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+const base = process.env.NODE_ENV==='production'?'https://forca-jogo.herokuapp.com':'https://forca-jogo.herokuapp.com'
+
+
 function Header() {
+    const logout = () => {
+        localStorage.removeItem("app-token")
+        window.location.href=`${base}/login`
+    }
+
     return (
         <>
             <Navbar bg="dark navbar-dark" expand="lg">
@@ -15,7 +23,7 @@ function Header() {
                         <Nav.Link><Link to='/admin/palavras'>Palavras</Link></Nav.Link>
                     </Nav>
                     <Nav>
-                        {/* <Button className='btn' >Sair</Button> */}
+                        <Button onClick={() => logout()} className='btn' >Sair</Button>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
